0a40839657b0edfb2a52d8a313151992bce4d419
VentusYue <shefzwy@163.com>
2020-04-16 19:43:10 -0400

    add comments

--- lib/gps2cart.m
+++ lib/gps2cart.m
@@ -1,15 +1,17 @@
 % modified on basis of Michael Kleder
 % https://www.mathworks.com/matlabcentral/fileexchange/7942-covert-lat-lon-alt-to-ecef-cartesian
 
+% check the following paper for a detailed explanation
+% http://papers.cumincad.org/data/works/att/a38d.content.03652.pdf
 
 function [x,y,z] = gps2cart(lat,lon,alt)
 
     % WGS84 ellipsoid constants:
-    a = 6378137;
-    e = 8.1819190842622e-2;
+    a = 6378137; % earth radius (semi-major axis of the ellipse)
+    e = 8.1819190842622e-2; % first eccentricity
     
     % intermediate calculation
-    % (prime vertical radius of curvature)
+    % N is vertical radius of curvature
     N = a ./ sqrt(1 - e^2 .* sin(lat).^2);
     
     % results:

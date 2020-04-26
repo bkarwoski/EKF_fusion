5b0727e32cabc95f889f7d5119ff659ba7973294
Kun Huang <huangkun@umich.edu>
2020-04-25 22:10:14 -0400

    dataloader is able to load gt; structure modified

--- lib/loadIMUandGPS.m
+++ lib/loadIMUandGPS.m
@@ -3,7 +3,11 @@
 % imu = [ts, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z],
 % It loads GPS data into
 % gps = [ts, gps_x, gps_y, gps_z]
+% It loads Ground Truth Poses into
+% gt = [ts, x_gt, y_gt, z_gt, omega_gt, phi_gt, kappa_gt];
 % NOTE: timestamps of IMU and GPS are not matched
+% After running this script, you can load IMU and GPS data directly from 
+% 'IMU_GPS_GT_data.mat'
 
 clear;
 
@@ -93,3 +97,43 @@ init_ts = min([init_ts_accel, init_ts_gyro, init_ts_gps]);
 accel(:, 1) = accel(:, 1) + (init_ts_accel - init_ts) / 1000000;
 gyro(:, 1) = gyro(:, 1) + (init_ts_gyro - init_ts) / 1000000;
 gps(:, 1) = gps(:, 1) + (init_ts_gps - init_ts) / 1000000;
+imu(:, 1) = imu(:, 1) + (init_ts_accel - init_ts) / 1000000;
+
+
+
+
+%% load Ground Truth.
+filename = '../AGZ_subset/Log Files/GroundTruthAGL.csv';
+delimiter = ',';
+startRow = 2;
+
+formatSpec = '%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+
+fileID = fopen(filename,'r');
+
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+fclose(fileID);
+
+imgid = dataArray{:, 1};
+x_gt = dataArray{:, 2};
+y_gt = dataArray{:, 3};
+z_gt = dataArray{:, 4};
+omega_gt = dataArray{:, 5};
+phi_gt = dataArray{:, 6};
+kappa_gt = dataArray{:, 7};
+
+ts = [];
+for i = 1:length(imgid)
+    ts = [ts; gps(imgid(i) + 1)];
+end
+
+gt = [ts, x_gt, y_gt, z_gt, omega_gt, phi_gt, kappa_gt];
+
+clearvars filename delimiter startRow formatSpec fileID dataArray ans;
+
+
+%% save data
+Filename = sprintf('IMU_GPS_GT_data.mat');
+save(Filename, 'accel', 'imu', 'gt');
+
